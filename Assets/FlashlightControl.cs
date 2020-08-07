c22c0f53bd0c9fa62dde2af516c98f260aad64e7
Jun Hyun Park <wordjun@naver.com>
2020-08-07 16:00:54 +0900

    August 7th

---
+++ Assets/FlashlightControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightControl : MonoBehaviour
+{
+    private Light myLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (myLight.enabled == false)
+                myLight.enabled = true;
+            else
+                myLight.enabled = false;
+        }
+    }
+}
