98dbc23268838deb824e1316bc153053486940a1
junhyunpark <wordjun@naver.com>
2020-11-22 23:00:37 +0900

    Nov 22nd

---
+++ Assets/Scripts/pickupPlates.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupPlates : MonoBehaviour
+{
+    //UI that says "pick up the plate"
+    public GameObject pickupPlateUI;
+    public GameObject plate;
+    public GameObject HUD;
+
+    public bool isInFrontOfPlate;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            isInFrontOfPlate = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            isInFrontOfPlate = false;
+    }
+    void Start()
+    {
+        pickupPlateUI.SetActive(false);
+        isInFrontOfPlate = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInFrontOfPlate)
+        {
+            pickupPlateUI.SetActive(true);
+            if (Input.GetKey(KeyCode.E))
+            {
+                plate.SetActive(false);//E키를 눌러 주우면 접시는 사라짐
+                //아이템창에 뜸
+
+            }
+        }
+        else
+        {
+            pickupPlateUI.SetActive(false);
+        }
+    }
+}
